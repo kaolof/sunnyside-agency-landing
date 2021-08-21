9bdd5bc6bba78410b0691a60694cba959747fd80
= <=>
2021-08-21 13:39:58 -0400

    fixing side menu

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+var li= document.getElementsByTagName("li");
+
 document.querySelector('.menu-btn').addEventListener('click', () =>{
     document.querySelector('.nav-bar').classList.toggle('show')
 });
+
+for(i=0; i<li.length; i++){
+    
+    li[i].addEventListener('click', () =>{
+        document.querySelector('.nav-bar').classList.toggle('show');
+
+    });
+}
